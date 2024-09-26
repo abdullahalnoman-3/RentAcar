76c125928a0d8e915c8405d1d2573b310780427a
AbdulaalNoman <167016116+AbdulaalNoman@users.noreply.github.com>
2024-09-26 15:13:35 +0600

    Update carEntry.blade.php

--- resources/views/admin/carEntry.blade.php
+++ resources/views/admin/carEntry.blade.php
@@ -1,13 +1,8 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Customer List') }}
-        </h2>
-    
-
 
     
-    <div>
+    <div class="">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Car entry') }}
         </h2>
@@ -18,22 +13,24 @@
         <label for="name">Car Name</label>
         <input type="text" class="form-control" id="name" name="name" placeholder="Enter car name" required>
     </div>
+    <br>
 
     <div class="form-group">
         <label for="brand">Brand</label>
         <input type="text" class="form-control" id="brand" name="brand" placeholder="Enter car brand" required>
     </div>
+    <br>
 
     <div class="form-group">
         <label for="model">Model</label>
         <input type="text" class="form-control" id="model" name="model" placeholder="Enter car model" required>
     </div>
-
+    <br>
     <div class="form-group">
         <label for="year">Year of Manufacture</label>
         <input type="number" class="form-control" id="year" name="year" placeholder="Enter year of manufacture" required>
     </div>
-
+    <br>
     <div class="form-group">
         <label for="car_type">Car Type</label>
         <select class="form-control" id="car_type" name="car_type" required>
@@ -43,12 +40,12 @@
             <option value="Coupe">Coupe</option>
         </select>
     </div>
-
+    <br>
     <div class="form-group">
         <label for="daily_rent_price">Daily Rent Price (in $)</label>
         <input type="number" class="form-control" id="daily_rent_price" name="daily_rent_price" placeholder="Enter daily rent price" step="0.01" required>
     </div>
-
+    <br>
     <div class="form-group">
         <label for="availability">Availability</label>
         <select class="form-control" id="availability" name="availability" required>
@@ -56,46 +53,21 @@
             <option value="0">Not Available</option>
         </select>
     </div>
-
+    <br>
+    <br>
     <div class="form-group">
         <label for="image">Car Image</label>
         <input type="file" class="form-control-file" id="image" name="image">
     </div>
-
+    <br>
+    <br>
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 
 
     </div>
 
-
-
     
-   <footer>
-    ok
-   </footer>
 
-  
-
-
-
-
-
-
-
-
-
-
-
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("i am customer") }}
-                </div>
-            </div>
-        </div>
-    </div>
     </x-slot>
 </x-app-layout>
